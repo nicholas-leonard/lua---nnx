326058a5aff6fe5efe63f63d22ac0455d23ea1b6
Nicholas Leonard <nick@nikopia.org>
2015-03-18 17:48:53 -0400

    initial commit for module:sharedClone

--- Module.lua
+++ Module.lua
@@ -1,25 +1,51 @@
 local Module = nn.Module
 
--- returns a list of modules
-function Module:listModules()
-   local function tinsert(to, from)
-      if torch.type(from) == 'table' then
-         for i=1,#from do
-            tinsert(to,from[i])
-         end
-      else
-         table.insert(to,from)
-      end
-   end
-   -- include self first
-   local modules = {self}
+Module.__parameters__ = {'weight', 'bias'}
+Module.__gradParameters__ = {'gradWeight', 'gradBias'}
+
+function Module:sharedClone(shareParams, shareGradParams)
+   local moduleClones, modules
    if self.modules then
-      for i=1,#self.modules do
-         local modulas = self.modules[i]:listModules()
-         if modulas then
-            tinsert(modules,modulas)
+      moduleClones = {}
+      for i,module in ipairs(self.modules) do
+         moduleClones[i] = module:sharedClone(shareParams, shareGradParams)
+      end
+      modules = self.modules
+      self.modules = nil
+   end
+   
+   local params = {}
+   if shareParams then
+      for i,paramName in ipairs(self.__parameters__) do
+         local param = self[paramName]
+         if param then
+            params[paramName] = param
+            self[paramName] = nil
          end
       end
    end
-   return modules
+   if shareGradParams then
+      for i,paramName in ipairs(self.__gradParameters__) do
+         local gradparam = self[paramName]
+         if gradParam then
+            params[paramName] = param
+            self[paramName] = nil
+         end
+      end
+   end
+   
+   -- clone everything but parameters and/or gradients
+   local clone = self:clone()
+   
+   for paramName, param in pairs(params) do
+      self[paramName] = param
+      -- TODO make this recursive (for table params)
+      clone[paramName] = param.new():set(param)
+   end
+   
+   if moduleClones then
+      self.modules = modules
+      clone.modules = moduleClones
+   end
+   return clone
 end
